c366b7681c85ea12f15c20b3f1ffd35974a8991d
Michael Patel <michael.patel94@gmail.com>
2019-08-13 09:10:20 -0400

    Adding file headers

---
+++ tf_1_12/train.py
@@ -0,0 +1,21 @@
+"""
+Michael Patel
+August 2019
+
+Python 3.6.5
+TensorFlow 1.12.0
+
+Project description:
+   To conduct GAN experimentation for learning purposes.
+   Based on the DCGAN paper: https://arxiv.org/pdf/1511.06434.pdf
+
+File description:
+    To run preprocessing and training algorithm
+
+Dataset: MNIST handwritten digits
+
+"""
+################################################################################
+# Imports
+
+################################################################################
