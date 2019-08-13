babc0d27ee193eab1440ca310ebb1c75b894cea7
Michael Patel <michael.patel94@gmail.com>
2019-08-13 09:20:29 -0400

    Adding results folder per run

--- tf_1_12/train.py
+++ tf_1_12/train.py
@@ -17,5 +17,27 @@ Dataset: MNIST handwritten digits
 """
 ################################################################################
 # Imports
+import os
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio  # generate gifs
+
+import tensorflow as tf
+
 
 ################################################################################
+# Main
+if __name__ == "__main__":
+    """
+    # enable eager execution
+    tf.enable_eager_execution()
+    """
+
+    # print out TF version
+    print("TF version: {}".format(tf.__version__))
+
+    # create directory for checkpoints, results
+    dir_name = "Results\\" + datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
