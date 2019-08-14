cccdd78f9fecdafa848f2628bc70d9d4e1c718cc
Michael Patel <michael.patel94@gmail.com>
2019-08-14 16:43:11 -0400

    Adding model defns

--- tf_1_12/model.py
+++ tf_1_12/model.py
@@ -17,5 +17,117 @@ Dataset: MNIST handwritten digits
 """
 ################################################################################
 # Imports
+import tensorflow as tf
+
 
 ################################################################################
+# Generator
+class Generator(tf.keras.Model):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        # Fully connected layer
+        self.fc = tf.keras.layers.Dense(
+            units=7*7*64,
+            use_bias=False
+        )
+
+        # Batchnormalization layer
+        self.batchnorm = tf.keras.layers.BatchNormalization()
+
+        # Convolutional layer #1
+        self.conv1 = tf.keras.layers.Conv2DTranspose(
+            filters=64,
+            kernel_size=(5, 5),
+            strides=(1, 1),
+            padding="same",
+            use_bias=False
+        )
+
+        # Convolutional layer #2
+        self.conv2 = tf.keras.layers.Conv2DTranspose(
+            filters=32,
+            kernel_size=(5, 5),
+            strides=(2, 2),
+            padding="same",
+            use_bias=False
+        )
+
+        # Convolutional layer #3
+        self.conv3 = tf.keras.layers.Conv2DTranspose(
+            filters=1,
+            kernel_size=(5, 5),
+            strides=(2, 2),
+            padding="same",
+            use_bias=False
+        )
+
+    # forward call
+    def call(self, x, training=True):
+        x = self.fc(x)
+        x = self.batchnorm(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = tf.reshape(x, shape=(-1, 7, 7, 64))
+
+        x = self.conv1(x)
+        x = self.batchnorm(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv2(x)
+        x = self.batchnorm(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = tf.nn.tanh(self.conv3(x))
+
+        return x
+
+
+################################################################################
+# Discriminator
+class Discriminator(tf.keras.Model):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        # Convolutional layer #1
+        self.conv1 = tf.keras.layers.Conv2D(
+            filters=64,
+            kernel_size=(5, 5),
+            strides=(2, 2),
+            padding="same"
+        )
+
+        # Convolutional layer #2
+        self.conv2 = tf.keras.layers.Conv2D(
+            filters=128,
+            kernel_size=(5, 5),
+            strides=(2, 2),
+            padding="same"
+        )
+
+        # Dropout layer
+        self.dropout = tf.keras.layers.Dropout(0.3)
+
+        # Flattening layer
+        self.flatten = tf.keras.layers.Flatten()
+
+        # Fully connected layer
+        self.fc = tf.keras.layers.Dense(
+            units=1
+        )
+
+    # forward pass
+    def call(self, x, training=True):
+        x = tf.nn.leaky_relu(self.conv1(x))
+
+        x = self.dropout(x, training=training)
+
+        x = tf.nn.leaky_relu(self.conv2(x))
+
+        x = self.dropout(x, training=training)
+
+        x = self.flatten(x)
+
+        x = self.fc(x)
+
+        return x
