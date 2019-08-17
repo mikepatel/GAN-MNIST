142ce9d14caa9f08ff6de219fead5c3156c83299
Michael Patel <michael.patel94@gmail.com>
2019-08-16 20:58:30 -0400

    Adding model training

--- tf_1_12_eager_exec/train.py
+++ tf_1_12_eager_exec/train.py
@@ -1,23 +1,18 @@
 """
 Michael Patel
-June 2019
+August 2019
 
 Python 3.6.5
-TF 1.12.0
+TensorFlow 1.12.0
 
 Project description:
+   To conduct GAN experimentation for learning purposes.
+   Based on the DCGAN paper: https://arxiv.org/pdf/1511.06434.pdf
 
-Datasets:
-    - MNIST
+File description:
+    To run preprocessing and training algorithm
 
-Notes:
-    - DCGAN: https://arxiv.org/pdf/1511.06434.pdf
-    - gif => imagemagick
-    - eager execution => no need for placeholders
-    - https://medium.com/coinmonks/8-things-to-do-differently-in-tensorflows-eager-execution-mode-47cf429aa3ad
-    - https://github.com/moono/tf-eager-on-GAN/blob/master/tf-1.x/dcgan_eager_tf_1_12.py
-
-Things to examine:
+Dataset: MNIST handwritten digits
 
 """
 ################################################################################
@@ -25,20 +20,107 @@ Things to examine:
 import os
 from datetime import datetime
 import numpy as np
+import time
 import matplotlib.pyplot as plt
+import imageio  # generate gifs
+from IPython import display
+import PIL
+import glob
 
 import tensorflow as tf
 
 from parameters import *
-from model import build_generator, build_discriminator
+from model import Generator, Discriminator
 
 
 ################################################################################
-# print image to screen
-def print_image(image):
-    plt.figure(figsize=(2, 2))  # 2x2 inches
-    plt.imshow(image, cmap="gray")
+# Discriminator Loss
+def disc_loss(real_output, generated_output):
+    real_loss = tf.losses.sigmoid_cross_entropy(
+        multi_class_labels=tf.ones_like(real_output),
+        logits=real_output
+    )
+
+    generated_loss = tf.losses.sigmoid_cross_entropy(
+        multi_class_labels=tf.zeros_like(generated_output),
+        logits=generated_output
+    )
+
+    total_loss = real_loss + generated_loss
+
+    return total_loss
+
+
+# Generator Loss
+def gen_loss(generated_output):
+    generated_loss = tf.losses.sigmoid_cross_entropy(
+        multi_class_labels=tf.ones_like(generated_output),
+        logits=generated_output
+    )
+
+    return generated_loss
+
+
+# Generate and Save Images
+def generate_and_save_images(model, epoch, test_input):
+    # set training parameter to False b/c don't want to train batchnorm layer when doing inference
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1),
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap="gray")
+        plt.axis("off")
+
+    plt.savefig("Epoch {:04d}.png".format(epoch))
     plt.show()
+
+
+# Training
+def train(dataset, epochs, noise_dim, d, g):
+    for epoch in range(epochs+1):
+        start = time.time()
+
+        for images in dataset:
+            # generate noise from uniform distribution
+            noise = tf.random_normal(shape=[BATCH_SIZE, noise_dim])
+
+            with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape:
+                generated_images = g(noise, training=True)
+
+                real_output = d(images, training=True)
+                generated_output = d(generated_images, training=True)
+
+                g_loss = gen_loss(generated_output)
+                d_loss = disc_loss(real_output, generated_output)
+
+            g_gradients = g_tape.gradient(g_loss, g.variables)
+            d_gradients = d_tape.gradient(d_loss, d.variables)
+
+            g_optimizer.apply_gradients(zip(g_gradients, g.variables))
+            d_optimizer.apply_gradients(zip(d_gradients, d.variables))
+
+        if epoch % 1 == 0:
+            display.clear_output(wait=True)
+
+            generate_and_save_images(g, epoch+1, random_vector_for_generation)
+
+        # save checkpoint
+        if (epoch+1) % 15 == 0:
+            checkpoint.save(file_prefix=checkpoint_prefix)
+
+        print("Time taken for epoch {} is {}s".format(epoch+1, time.time()-start))
+
+    # generate gif after final epoch
+    display.clear_output(wait=True)
+    generate_and_save_images(g, epochs, random_vector_for_generation)
+
+
+# Display an image
+def display_image(epoch_number):
+    image_file = os.path.join(os.getcwd(), "image_at_epoch_{:04d}.png".format(epoch_number))
+    return PIL.Image.open(image_file)
 
 
 ################################################################################
@@ -55,62 +137,86 @@ if __name__ == "__main__":
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
-    #
-    tf.reset_default_graph()
-    tf.keras.backend.set_learning_phase(1)  # 1=train, 0=test
-
     # ----- ETL ----- #
     # ETL = Extraction, Transformation, Load
     (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
 
-    '''
-    # show a training image before Preprocessing Transformation
-    i = np.random.randint(low=0, high=len(train_images))
-    random_image = train_images[i]
-    print_image(random_image)
-    quit()
-    '''
+    print("Shape of training images before reshape: {}".format(train_images.shape))
 
-    # Reshape: (28, 28) => (28, 28, 1)
-    print("Shape of training images before reshape: {}".format(train_images[0].shape))
-
+    # Reshape: (28, 28) --> (28, 28, 1)
     train_images = train_images.reshape(
         train_images.shape[0], IMAGE_ROWS, IMAGE_COLS, IMAGE_CHANNELS
     ).astype("float32")
 
-    print("Shape of training images after reshape: {}".format(train_images[0].shape))
+    print("Shape of training images after reshape: {}".format(train_images.shape))
 
-    # Normalize images to [-1, 1] - tanh activation
+    # Normalize images to [-1, 1] --- tanh activation
     train_images = (train_images - 127.5) / 127.5
 
-    # use tf.data.Dataset to create batches and shuffle => TF model
+    # use tf.data.Dataset to create batches and shuffle --> data pipeline to TF model
     train_dataset = tf.data.Dataset.from_tensor_slices(train_images)
     train_dataset = train_dataset.shuffle(buffer_size=BUFFER_SIZE)
     train_dataset = train_dataset.batch(batch_size=BATCH_SIZE)
 
-    print("Shape of batches: {}".format(train_dataset))
+    print("Shape of batches: {}".format(train_dataset.output_shapes))
 
     # ----- MODEL ----- #
-    """
-    # Placeholders
-    real_image_pl = tf.placeholder(dtype=tf.float32,
-                                   shape=[None, IMAGE_ROWS, IMAGE_COLS, IMAGE_CHANNELS],
-                                   name="real_image_placeholder")
+    g = Generator()
+    d = Discriminator()
 
-    noise_pl = tf.placeholder(dtype=tf.float32,
-                              shape=[None, Z_DIM],
-                              name="noise_placeholder")
+    # defun gives 10s per epoch performance boost
+    g.call = tf.contrib.eager.defun(g.call)
+    d.call = tf.contrib.eager.defun(d.call)
 
-    #
-    g_out = build_generator(noise_pl, reuse=False)
+    # Optimizers
+    d_optimizer = tf.train.AdamOptimizer(learning_rate=D_LEARNING_RATE)
+    g_optimizer = tf.train.AdamOptimizer(learning_rate=G_LEARNING_RATE)
 
-    d_real_out = build_discriminator(real_image_pl, reuse=False)
-
-    d_fake_out = build_discriminator(g_out, reuse=True)
-    """
-    g = build_generator(Z_DIM)
-    d = build_discriminator()
+    # Checkpoints
+    checkpoint_dir = "./training_checkpoints"
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(
+        g_optimizer=g_optimizer,
+        d_optimizer=d_optimizer,
+        g=g,
+        d=d
+    )
 
     # ----- TRAINING ----- #
+    # keep random vector constant for generation to track gan improvement easier
+    random_vector_for_generation = tf.random_normal(shape=[NUM_GEN_IMAGES, NOISE_DIM])
 
-    # ----- GENERATE ----- #
+    train(train_dataset, NUM_EPOCHS, NOISE_DIM, d, g)
+
+    # Restore latest checkpoint
+    checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir=checkpoint_dir))
+
+    # Display an image
+    display_image(NUM_EPOCHS)
+
+    # Generate gif of all saved images
+    with imageio.get_writer("dcgan.gif", mode="I") as writer:
+        filenames = glob.glob("image*.png")
+        filenames = sorted(filenames)
+
+        last = -1
+
+        for i, filename in enumerate(filenames):
+            frame = 2*(i**0.5)
+
+            if round(frame) > round(last):
+                last = frame
+
+            else:
+                continue
+
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+        image = imageio.imread(filename)
+        writer.append_data(image)
+
+    # hack to display gif inside notebook
+    os.system("cp dcgan.gif dcgan.gif.png")
+
+    display.Image(filename="dcgan.gif.png")
