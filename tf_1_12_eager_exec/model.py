9ab53fd275bf388aa6e13d04811f83d3ffa2ffaa
Michael Patel <michael.patel94@gmail.com>
2019-08-16 20:58:09 -0400

    Adding model fns

--- tf_1_12_eager_exec/model.py
+++ tf_1_12_eager_exec/model.py
@@ -1,142 +1,141 @@
 """
 Michael Patel
-June 2019
+August 2019
 
 Python 3.6.5
-TF 1.12.0
+TensorFlow 1.12.0
 
 Project description:
+   To conduct GAN experimentation for learning purposes.
+   Based on the DCGAN paper: https://arxiv.org/pdf/1511.06434.pdf
 
-Datasets:
-    - MNIST
+File description:
+    To build model definitions for Generator and Discriminator
 
-Notes:
-    - DCGAN: https://arxiv.org/pdf/1511.06434.pdf
-    - http://superfluoussextant.com/making-gifs-with-python.html
-    - https://tomroelandts.com/articles/how-to-create-animated-gifs-with-python
-
-Things to examine:
+Dataset: MNIST handwritten digits
 
 """
 ################################################################################
 # Imports
 import tensorflow as tf
-from parameters import *
+
+from parameters import DROPOUT_RATE
 
 
 ################################################################################
 # Generator
-def build_generator():
-    m = tf.keras.Sequential()
+class Generator(tf.keras.Model):
+    def __init__(self):
+        super(Generator, self).__init__()
 
-    # Input layer
-    m.add(tf.keras.layers.Dense(
-        units=7*7*512,
-        input_shape=(Z_DIM, )
-    ))
-    m.add(tf.keras.layers.BatchNormalization())
-    m.add(tf.keras.layers.LeakyReLU())
-    #m.add(tf.keras.layers.ReLU())
+        # Fully connected layer
+        self.fc = tf.keras.layers.Dense(
+            units=7*7*64,
+            use_bias=False
+        )
 
-    # Reshape layer
-    m.add(tf.keras.layers.Reshape((7, 7, 512)))
+        # Batchnormalization  #1
+        self.batchnorm1 = tf.keras.layers.BatchNormalization()
 
-    # Convolutional Layer 1
-    m.add(tf.keras.layers.Conv2DTranspose(
-        filters=512,
-        kernel_size=(5, 5),
-        strides=(1, 1),
-        padding="same"
-    ))
-    m.add(tf.keras.layers.BatchNormalization())
-    m.add(tf.keras.layers.LeakyReLU())
+        # Convolutional layer #1
+        self.conv1 = tf.keras.layers.Conv2DTranspose(
+            filters=64,
+            kernel_size=(5, 5),
+            strides=(1, 1),
+            padding="same",
+            use_bias=False
+        )
 
-    # Convolutional Layer 2
-    m.add(tf.keras.layers.Conv2DTranspose(
-        filters=256,
-        kernel_size=(5, 5),
-        strides=(1, 1),
-        padding="same"
-    ))
-    m.add(tf.keras.layers.BatchNormalization())
-    m.add(tf.keras.layers.LeakyReLU())
+        # Batchnormalization layer #2
+        self.batchnorm2 = tf.keras.layers.BatchNormalization()
 
-    # Convolutional Layer 3
-    m.add(tf.keras.layers.Conv2DTranspose(
-        filters=128,
-        kernel_size=(5, 5),
-        strides=(2, 2),
-        padding="same"
-    ))
-    m.add(tf.keras.layers.BatchNormalization())
-    m.add(tf.keras.layers.LeakyReLU())
+        # Convolutional layer #2
+        self.conv2 = tf.keras.layers.Conv2DTranspose(
+            filters=32,
+            kernel_size=(5, 5),
+            strides=(2, 2),
+            padding="same",
+            use_bias=False
+        )
 
-    # Convolutional Layer 4
-    m.add(tf.keras.layers.Conv2DTranspose(
-        filters=1,
-        kernel_size=(5, 5),
-        strides=(2, 2),
-        padding="same",
-        activation=tf.keras.activations.tanh
-    ))
+        # Batchnormalization layer #3
+        self.batchnorm3 = tf.keras.layers.BatchNormalization()
 
-    # configure model training
-    m.compile(
-        loss=tf.keras.losses.binary_crossentropy,
-        optimizer=tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5),
-        metrics=["accuracy"]
-    )
+        # Convolutional layer #3
+        self.conv3 = tf.keras.layers.Conv2DTranspose(
+            filters=1,
+            kernel_size=(5, 5),
+            strides=(2, 2),
+            padding="same",
+            use_bias=False
+        )
 
-    m.summary()
+    # forward call
+    def call(self, x, training=True):
+        x = self.fc(x)
+        x = self.batchnorm1(x, training=training)
+        x = tf.nn.relu(x)
 
-    return m
+        x = tf.reshape(x, shape=(-1, 7, 7, 64))
+
+        x = self.conv1(x)
+        x = self.batchnorm2(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv2(x)
+        x = self.batchnorm3(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = tf.nn.tanh(self.conv3(x))
+
+        return x
 
 
 ################################################################################
 # Discriminator
-def build_discriminator():
-    m = tf.keras.Sequential()
+class Discriminator(tf.keras.Model):
+    def __init__(self):
+        super(Discriminator, self).__init__()
 
-    # Convolutional Layer 1
-    m.add(tf.keras.layers.Conv2D(
-        filters=64,
-        kernel_size=(5, 5),
-        strides=(2, 2),
-        padding="same",
-        input_shape=(28, 28, 1)
-    ))
-    m.add(tf.keras.layers.LeakyReLU())
+        # Convolutional layer #1
+        self.conv1 = tf.keras.layers.Conv2D(
+            filters=64,
+            kernel_size=(5, 5),
+            strides=(2, 2),
+            padding="same"
+        )
 
-    #
-    m.add(tf.keras.layers.Dropout(rate=0.3))  # fraction of input units to drop
+        # Convolutional layer #2
+        self.conv2 = tf.keras.layers.Conv2D(
+            filters=128,
+            kernel_size=(5, 5),
+            strides=(2, 2),
+            padding="same"
+        )
 
-    # Convolutional Layer 2
-    m.add(tf.keras.layers.Conv2D(
-        filters=128,
-        kernel_size=(5, 5),
-        strides=(2, 2),
-        padding="same"
-    ))
-    m.add(tf.keras.layers.LeakyReLU())
+        # Dropout layer
+        self.dropout = tf.keras.layers.Dropout(rate=DROPOUT_RATE)
 
-    #
-    m.add(tf.keras.layers.Dropout(rate=0.3))
+        # Flattening layer
+        self.flatten = tf.keras.layers.Flatten()
 
-    #
-    m.add(tf.keras.layers.Flatten())
+        # Fully connected layer
+        self.fc = tf.keras.layers.Dense(
+            units=1
+        )
 
-    #
-    m.add(tf.keras.layers.Dense(
-        units=1
-    ))
+    # forward pass
+    def call(self, x, training=True):
+        x = tf.nn.leaky_relu(self.conv1(x))
 
-    # configure model training
-    m.compile(
-        loss=tf.keras.losses.binary_crossentropy,
-        optimizer=tf.train.AdamOptimizer(learning_rate=0.0002, beta1=0.5),
-        metrics=["accuracy"]
-    )
+        x = self.dropout(x, training=training)
 
-    m.summary()
+        x = tf.nn.leaky_relu(self.conv2(x))
 
-    return m
+        x = self.dropout(x, training=training)
+
+        x = self.flatten(x)
+
+        x = self.fc(x)
+
+        return x
